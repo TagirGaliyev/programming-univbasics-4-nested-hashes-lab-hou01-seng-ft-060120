f46084a8fd4963803254d81b1ee1e92914775fcc
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-14 20:06:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
       }
     }
     programmer_hash[:alan_kay][:known_for] = "GUI"
-  #  programmer_hash
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -132,4 +132,5 @@ def adding_to_dennis
       }
     }
     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash
 end
